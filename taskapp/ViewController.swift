debff32ecad3d5797913bd695b956b9dd73be21b
mizuki.nakajima <m173305s@st.u-gakugei.ac.jp>
2019-03-07 10:21:52 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Nakajima Mizuki on 2019/03/07.
+//  Copyright © 2019 Nakajima Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
